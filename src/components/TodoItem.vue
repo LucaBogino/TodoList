93c9beaa371e464b16cc3c4a98d2501791c2da59
Luca Bogino <luca.bogino.ext@alten.it>
2022-09-30 16:16:30 +0200

    created Todo's components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="description">
+        {{ todo.description }}
+        <span class="created-on">
+          (created on {{ moment(todo.createdOn).format('DD-MM-YYYY HH:mm') }})
+        </span>
+      </div>
+      <div class="todo-action">
+        <span
+          class="fa"
+          :class="[todo.finishedOn ? 'fa-check-square checked' : 'fa-square']"
+          @click="changeTodo(todo)"></span>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'TodoItem',
+    props: ['todo'],
+    emits: ["change-todo"],
+    data() {
+        return{
+            moment
+        }
+    },
+    methods: {
+        changeTodo() {
+            this.$emit('change-todo', this.todo)
+        },
+    }
+}
+</script>
+
+<style scoped>
+  .description {
+    font-size: 1rem;
+  }
+  .description > .created-on {
+    display: block;
+    font-size: .9rem;
+    color: #999;
+    font-style: italic;
+  }
+  .todo-action {
+    font-size: 1.3rem;
+  }
+  .checked {
+    color: #3eaf7c
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,73 @@
+<template>
+    <TodoForm @add-todo="addTodo"/>
+
+    <div class="todo-list">
+        <div class="todo-item" v-for="todo in todos" :key="todo.id">
+          <TodoItem 
+            :key="todo.id" 
+            :todo="todo"
+            @change-todo="changeTodo"></TodoItem>
+        </div>
+    </div>
+</template>
+  
+<script>
+  import TodoForm from './TodoForm.vue';
+  import TodoItem from './TodoItem.vue';
+
+  export default {
+    components: {
+        TodoForm,
+        TodoItem        
+    },
+    data() {
+      return {
+          todos: [
+                    {
+                      id: 1664543434837,
+                      description: 'Follow Luca Bogino on GitHub',
+                      createdOn: 1664543434837,
+                      finishedOn: null
+                    },
+          ],
+      }
+  },
+  methods: {
+      changeTodo(todo) {
+          todo.finishedOn = !todo.finishedOn ? new Date() : null
+      },
+      addTodo(newTodo) {
+        const todo = {
+          id: Date.now(),
+          description: newTodo,
+          createdOn: new Date().getTime(),
+          finishedOn: null
+        }
+
+        this.todos.push(todo)
+      }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-list {
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    align-content: center;
+    align-self: center;
+  }
+  .todo-item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    align-content: space-between;
+    justify-content: space-between;
+    margin: 10px 10px;
+    border: 1px solid #c9c9c9;
+    padding: 20px;
+    width: 40%;
+    text-align: left;
+  }
+</style>
